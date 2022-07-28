b46c00a92924ab24abdd8bab8e140e238c6b4c75
James Behr <jamesbehr@gmail.com>
2022-07-28 22:22:39 +0200

    nvim: duskfox colorscheme

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
     use("nvim-telescope/telescope.nvim")
     use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
     use("nvim-telescope/telescope-file-browser.nvim")
+    use("EdenEast/nightfox.nvim")
 
     if packer_bootstrap then
         require('packer').sync()
