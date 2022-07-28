6d42261a95c4f651e2973ed59ac4bc5d0156c1b9
James Behr <jamesbehr@gmail.com>
2022-07-28 22:16:18 +0200

    nvim: add Telescope binding

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -21,6 +21,8 @@ require("telescope").setup({
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('file_browser')
 
+nnoremap("<leader>t", "<cmd>Telescope<cr>")
+
 nnoremap("<leader><leader>", function()
     local _, ret, _ = utils.get_os_command_output({ 'git', 'rev-parse', '--is-inside-work-tree' })
     if ret == 0 then
