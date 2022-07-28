ac87bc9ba97abb70dd1baf81a8e1bb783696b24d
James Behr <jamesbehr@gmail.com>
2022-07-28 21:23:16 +0200

    nvim: bootstrap packer

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,7 +1,18 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
-use("wbthomason/packer.nvim")
-use("nvim-lua/plenary.nvim")
-use("nvim-lua/popup.nvim")
-use("nvim-telescope/telescope.nvim")
-use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
+    use("wbthomason/packer.nvim")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-lua/popup.nvim")
+    use("nvim-telescope/telescope.nvim")
+    use({"nvim-telescope/telescope-fzf-native.nvim", run = "make"})
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
