5c2ce4043bce6a40e199e8318aa69530f2c14e5c
James Behr <jamesbehr@gmail.com>
2022-08-05 16:04:04 +0200

    nvim: fix completion navigation
    `nvim-cmp` doesn't provide any default mappings. You need to manually
    configure it to use the preset mapping to allow navigating the
    completion window with `C-n` and `C-p`.

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -6,6 +6,12 @@ cmp.setup({
             require('luasnip').lsp_expand(args.body)
         end,
     },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "buffer" },
